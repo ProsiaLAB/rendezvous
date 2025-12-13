ed88cb09de5c0eca5d85223122784c17121aa34c
kmaitreys <km.maitrey@gmail.com>
2025-12-14 00:17:27 +0530

    [ex] add basic example

---
+++ examples/001_bouncing_balls.rs
@@ -0,0 +1,41 @@
+use rendezvous::{
+    collision::CollisionResolver,
+    integrator::Integrator,
+    leapfrog::LeapFrog,
+    particle::Particle,
+    rendezvous::{Collision, Gravity, Simulation},
+};
+
+fn main() {
+    let mut sim = Simulation::init(Integrator::LeapFrog(LeapFrog));
+
+    sim.dt = 1e-2;
+    sim.gravity = Gravity::Basic;
+    sim.collision = Collision::Direct;
+    sim.collision_resolve = CollisionResolver::HardSphere.into();
+
+    sim.configure_box(3.0, 1, 1, 1);
+
+    // Initial conditions
+    let p = Particle {
+        x: 1.0,
+        y: 1.0,
+        z: 1.0,
+        m: 1.0,
+        r: 0.1,
+        ..Default::default()
+    };
+    sim.add(p);
+
+    let p = Particle {
+        x: -1.0,
+        y: -1.0,
+        z: -1.0,
+        m: 1.0,
+        r: 0.1,
+        ..Default::default()
+    };
+    sim.add(p);
+
+    sim.integrate(f64::INFINITY);
+}
