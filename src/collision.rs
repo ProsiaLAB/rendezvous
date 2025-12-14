4cf78d1bd6d13fe8d51fa41e3a84c660c1f055bc
kmaitreys <km.maitrey@gmail.com>
2025-12-14 12:01:15 +0530

    [feat] plan is to write methods for clean monomorphic dispatch

--- src/collision.rs
+++ src/collision.rs
@@ -3,3 +3,29 @@ pub enum CollisionResolver {
     Halt,
     Merge,
 }
+
+pub enum ResolutionOutcome {
+    RemoveNone,
+    RemoveFirst,
+    RemoveSecond,
+    RemoveBoth,
+}
+
+impl CollisionResolver {
+    pub fn resolve(&self) -> ResolutionOutcome {
+        match self {
+            CollisionResolver::HardSphere => {
+                // Implement hard sphere collision resolution
+                todo!()
+            }
+            CollisionResolver::Halt => {
+                // Implement halt on collision
+                todo!()
+            }
+            CollisionResolver::Merge => {
+                // Implement merge on collision
+                todo!()
+            }
+        }
+    }
+}
